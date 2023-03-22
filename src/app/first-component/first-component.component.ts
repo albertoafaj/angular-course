29d7ae9d98bb4b1878614f51880e089be109e98d
Alberto Araújo <albertoafaj@hotmail.com>
2023-03-22 17:27:43 -0300

    feat: create a basic component

---
+++ src/app/first-component/first-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-first-component",
+  template: "<h2>Primeiro Componente",
+  styles: ["h2 {color: red}"]
+})
+export class FirstComponentComponent {
+
+};
