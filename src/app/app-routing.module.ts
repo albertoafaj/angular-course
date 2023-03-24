19619f925497685d4efd1229bcd97cfe13d24c77
Alberto Araújo <albertoafaj@hotmail.com>
2023-03-24 09:05:01 -0300

    feat: add route with query params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstPageComponent } from './first-page/first-page.component';
 import { SecondPageComponent } from './second-page/second-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageParamsComponent } from './page-params/page-params.component';
 
 const routes: Routes = [
   { path: "page-one", component: FirstPageComponent },
   { path: "page-two", component: SecondPageComponent },
   { path: "", redirectTo: "page-one", pathMatch: "full" },
+  { path: "page-params/:id", component: PageParamsComponent },
   { path: "**", component: PageNotFoundComponent },
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { LifeCicleParentComponent } from './life-cicle-parent/life-cicle-parent.
 import { FirstPageComponent } from './first-page/first-page.component';
 import { SecondPageComponent } from './second-page/second-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageParamsComponent } from './page-params/page-params.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     LifeCicleParentComponent,
     FirstPageComponent,
     SecondPageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    PageParamsComponent
   ],
   imports: [
     BrowserModule,
